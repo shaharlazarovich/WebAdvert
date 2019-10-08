8eec75f2903f4da23c15d02cff878de9edc30a5c
Shahar Lazarovich <shaharlazarovich@gmail.com>
2019-10-08 06:59:13 +0300

    Add project files.

---
+++ WebAdvert.AdvertApi/AdvertApi/Startup.cs
@@ -0,0 +1,69 @@
+﻿using AdvertApi.HealthChecks;
+using AdvertApi.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AdvertApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper();
+            services.AddTransient<IAdvertStorageService, DynamoDBAdvertStorage>();
+            services.AddTransient<StorageHealthCheck>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            services.AddHealthChecks().AddCheck<StorageHealthCheck>("Storage");
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllOrigin", policy=> policy.WithOrigins("*").AllowAnyHeader());
+            });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Web Advertisement Apis",
+                    Version = "version 1",
+                    Contact = new Contact
+                    {
+                        Name = "Aref Karimi",
+                        Email = "a@b.com"
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseHealthChecks("/health");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Advert Api");
+            });
+            app.UseMvc();
+        }
+    }
+}
---
+++ WebAdvertWeb/WebAdvert.Web/WebAdvert.Web/Models/Accounts/SignUpModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignUpModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password  must be at least 6 characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Passowrd", ErrorMessage = "Passowrd and its confirmation do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
